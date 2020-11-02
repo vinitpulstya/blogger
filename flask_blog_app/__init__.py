e9538cc3e7d2e507a545273c455c77653da56bc3
Vinit Sharma <vinitsharma9876@gmail.com>
2020-11-02 23:13:54 +0530

    Added pagination, sort by latest post and reset password by sending reset link on user's mail

--- flask_blog_app/__init__.py
+++ flask_blog_app/__init__.py
@@ -1,7 +1,9 @@
+import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
+from flask_mail import Mail, Message
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '00e6e9abfb7f323f265efcfd93947a48'
@@ -11,5 +13,13 @@ bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
 login_manager.login_message_category = 'info'
-                
+app.config['MAIL_SERVER'] = 'smtp.gmail.com'
+app.config['MAIL_PORT'] = 465
+app.config['MAIL_USE_SSL'] = True
+app.config['MAIL_USERNAME'] = os.environ.get('USER_MAIL')
+app.config['MAIL_PASSWORD'] = os.environ.get('USER_PASS')
+app.config['MAIL_USE_TLS'] = False
+
+mail = Mail(app)
+
 from flask_blog_app import routes
